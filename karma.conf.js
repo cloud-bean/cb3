bcfb1569cbaf8b11fae6bff37638e4e19a560893
hygkui <tom@erealmsoft.com>
2015-12-09 01:06:56 +0800

    use jasmine for karma's framework
    $karma start
    open chrome 0.0.0.0:9876/
    then see the test results.

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test/client/*.js',
+      'test/client/*.js'
     ],
 
 
@@ -61,10 +61,10 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultanous
     concurrency: Infinity
   })
-}
+};
--- test/server/specs/helloSpec.js
+++
@@ -1,7 +0,0 @@
-var expect = require('expect.js');
-
-describe('hello Spec test', function () {
-  it('should say hello', function () {
-     expect('hello').to.equal('hello');
-  });
-})
