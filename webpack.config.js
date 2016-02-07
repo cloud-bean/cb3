a0a5d20e6bf4f02936dcf88b6d2e522b9bb8836c
hygkui <tom@erealmsoft.com>
2016-02-07 23:19:01 +0800

    adjust config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,50 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 const autoprefixer = require('autoprefixer');
 const path = require('path');
-
 module.exports = {
-	context: __dirname,
-	devtool: 'inline-source-map',
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./src/index.jsx'
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'react-hot!babel'
-			},
-			{
-				test: /(\.scss|\.css)$/,
-				loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
-			},
-			{
-				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-				loader: 'url-loader?limit=81920'
-			}
-		]
-	},
-	postcss: [autoprefixer],
-	resolve: {
-		extensions: ['', '.js', '.jsx', '.scss']
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	devServer: {
-		contentBase: './dist',
-		hot:true
-	},
-	plugins: [
-		new ExtractTextPlugin('bundle.css', { allChunks: true }),
-		new webpack.HotModuleReplacementPlugin(),
-  //  new webpack.optimize.UglifyJsPlugin({minimize: true}),
-  //  new webpack.optimize.CommonsChunkPlugin('common.js')
-	]
+    context: __dirname,
+    devtool: 'inline-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.jsx'
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'react-hot!babel'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.scss$/,
+                loader: "style!css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap"
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=81920'
+            }
+        ]
+    },
+    postcss: [autoprefixer],
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.scss']
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css', {allChunks: true}),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
